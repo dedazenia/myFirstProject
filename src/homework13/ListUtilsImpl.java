e423b0ad319f9b9a78d711bb5d8b54246eade45c
dedazenia <Wuvwux-hizzem-9wotfo>
2020-01-10 18:37:08 +0300

    первый коммит проекта

--- src/homework13/ListUtilsImpl.java
+++ src/homework13/ListUtilsImpl.java
@@ -6,36 +6,19 @@ public class ListUtilsImpl implements ListUtils {
 
     @Override
     public List<String> asList(String... strings) throws IllegalArgumentException {
-        List<String> result = new LinkedList<>();
+        ArrayList<String> result = new ArrayList<>();
         result.addAll(Arrays.asList(strings));
-
-//        List<String> result = new LinkedList<>();
-//        for (int i = 0; i < strings.length; i++) {
-//            result.add(strings[i]);
-//        }
         return result;
     }
 
 
     @Override
     public List<Double> sortedList(List<Double> data) throws IllegalArgumentException {
-        if (data==null){
-            throw new IllegalArgumentException();
-        }
-        List<Double> result=new ArrayList<Double>(data);
-        result.sort(DecComoarator);
+        ArrayList<Double> result = new ArrayList<>(data);
+        Collections.sort(result);
+        Collections.reverse(result);
         return result;
     }
-
-    static final Comparator<Double> DecComoarator=new Comparator<Double>() {
-        @Override
-        public int compare(Double o1, Double o2) {
-            return o2.compareTo(o1);
-        }
-
-    };
-
-
 
 
 }
